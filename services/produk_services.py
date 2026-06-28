a32d6706eb17b63db0706e8c56b719a99cf3bafb
Sabrosa Noval <sabrosanov06@gmail.com>
2026-06-28 22:48:48 +0700

    update produk services

--- services/produk_services.py
+++ services/produk_services.py
@@ -1,30 +1,43 @@
 from typing import Any, List, Optional
-
-try:
-    from repository.produk_repository import ProdukRepository
-except ImportError:
-    ProdukRepository = None
-
+from repository.produk_repository import CroissantRepository, KueKeringRepository, RotiManisRepository
 
 class ProdukService:
+    def __init__(self):
+        self.repo_croissant = CroissantRepository()
+        self.repo_kuekering = KueKeringRepository()
+        self.repo_rotimanis = RotiManisRepository()
 
-    def __init__(self, repository: Optional[ProdukRepository] = None): # type: ignore
-        self.repository = repository or (ProdukRepository() if ProdukRepository else None)
-    
-    def tambah_produk(self, produk: Any) -> bool:
-        if self.cari_produk_by_kode(produk.kode) is not None:
+    def tambah_produk(self, kategori: str, jenis : str, nama: str, kode: str, bahan: list, biaya: float, harga: float) -> bool:
+        if self.cari_produk_by_kode(kode) is not None:
+            print(f"Produk dengan kode {kode} sudah ada.")
             return False
-        self.repository.tambah(produk)
+        
+        kategori = kategori.lower()
+        if kategori == "croissant":
+            return self.repo_croissant.tambah_produk(jenis, nama, kode, bahan, biaya, harga)
+        elif kategori == "kue kering":
+            return self.repo_kuekering.tambah_produk(jenis, nama, kode, bahan, biaya, harga)
+        elif kategori == "roti_manis":
+            return self.repo_rotimanis.tambah_produk(jenis, nama, kode, bahan, biaya, harga)
+        else:
+            print("Gagal: Kategori tidak valid. Pilih 'croissant', kue_kering', atau 'roti_manis'.")
+            return False
+        
         return True
     
     def ambil_semua_produk(self) -> List[Any]:
-        """
-        buat dipakai di menu "tampilkan semua produk".
-        """
-        return self.repository.get_semua()
+        semua_produk = []
+        semua_produk.extend(self.repo_croissant.daftar_croissant())
+        semua_produk.extend(self.repo_kuekering.daftar_kuekering())
+        semua_produk.extend(self.repo_rotimanis.daftar_rotimanis())
+        return semua_produk
     
     def cari_produk_by_kode(self, kode: str) -> Optional[Any]:
-        for produk in self.repository.get_semua():
-            if produk.kode == kode:
-                return produk
-        return None
+        produk = self.repo_croissant.cari_kode(kode)
+        if produk: return produk
+        
+        produk = self.repo_kuekering.cari_kode(kode)
+        if produk: return produk
+        
+        produk = self.repo_rotimanis.cari_kode(kode)
+        return produk
